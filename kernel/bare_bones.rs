701121125bfa72710114a10750190619416f14bd
Mark Mansi <markm@cs.wisc.edu>
2018-04-19 18:13:41 -0500

    working again

--- kernel/bare_bones.rs
+++ kernel/bare_bones.rs
@@ -5,15 +5,21 @@
 use core::fmt;
 
 use debug::Debug;
+use machine::cli;
 
 // For bare-bones rust
 #[lang = "eh_personality"]
-fn eh_personality() {}
+#[no_mangle]
+pub extern "C" fn eh_personality() {}
 
 /// This function is used by `panic!` to display an error message.
 #[lang = "panic_fmt"]
+#[no_mangle]
 pub extern "C" fn rust_begin_unwind(args: fmt::Arguments, file: &'static str, line: u32) -> ! {
     use core::fmt::Write;
+    unsafe {
+        cli();
+    } // we should no be interrupting any more
     printk!("\nPanic at {}:{}: ", file, line);
     let _ = Debug.write_fmt(args);
     printk!("\n");
